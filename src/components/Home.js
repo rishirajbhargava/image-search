98cbba33775feb8cbc0a13faf8d2ee0c25878e12
rishirajbhargava <rishiraj.bhargava00@gmail.com>
2023-06-10 16:50:51 +0530

    responsiveness added

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import './styles/Home.css'
-import ScaleLoader  from "react-spinners/ScaleLoader";
+import ScaleLoader from "react-spinners/ScaleLoader";
 import logo from '../static//Unsplash_wordmark_logo.svg'
 function Home() {
 
@@ -20,22 +20,22 @@ function Home() {
         handleSubmit(e)
     }
 
-   
+
     const handleSubmit = async (e) => {
         e.preventDefault();
         setLoading(true)
         try {
             const response = await fetch(`https://api.unsplash.com/search/photos?page=${page}&query=${query}&client_id=${process.env.REACT_APP_UNSPLASH_KEY}&per_page=12`)
             const data = await response.json()
-           
+
             setImages(data.results)
             setLoading(false)
             setHasMore(true)
         } catch (error) {
-            
+
             setError(true)
             setLoading(false)
-           
+
         }
     }
 
@@ -43,76 +43,88 @@ function Home() {
 
 
 
-  return (
-    <div className='home'>
+    return (
+        <div className='home'>
 
             <div className='container'>
                 <h1>Image Search</h1>
                 <p>Powered by <img src={logo} alt='logo' /></p>
                 <form onSubmit={searchImages} >
-                <input type='text' name='query' required placeholder='Search for anything... ' onChange={handleChange}/>
-                <button>Search</button>
+                    <input type='text' name='query' required placeholder='Search for anything... ' onChange={handleChange} />
+                    <button>Search</button>
                 </form>
-            
+
             </div>
 
 
-                {loading ?  <ScaleLoader 
-                      color="#FFF"
-                        height={20}
-                        width={3}
-                    aria-label="Loading Spinner"
-                    data-testid="loader"
-                />:
+            {loading ? <ScaleLoader
+                color="#FFF"
+                height={20}
+                width={3}
+                aria-label="Loading Spinner"
+                data-testid="loader"
+            /> :
 
                 <>
-                
-                
-               { error || (images.length===0 && hasMore===true) ? <div className='error'>Something went wrong...</div>
-                :  <div className='results'>
 
-                
-                
-                {images.map((image) => {
-                    return (
+                    {!hasMore && <div className='info' >
+                        <p > A Project by Rishi Raj Bhargava</p>
+                        <div>
 
-                       <div className='image-container' key={image.id}> 
-                        <div className='image'>
-                        <img src={image.urls.small} alt={image.alt_description}/>
+                   
+                        <a aria-label="linkedin" rel="noreferrer" target="_blank" href="https://www.linkedin.com/in/rishirajbhargava/" className="nav-socials-links">
+                            <i className="fa-brands fa-linkedin"></i></a>
+                        <a aria-label="github" rel="noreferrer" target="_blank" href="https://www.github.com/rishirajbhargava/" className="nav-socials-links">
+                            <i className="fa-brands fa-github"></i>
+                        </a>
                         </div>
-                            <div className='text'>
-                                    <h2>{image.alt_description}</h2>
-                                    <p>By: {image.user.name}</p>
-                                    <a href={image.urls.raw}>Download</a>
+                    </div>}
+                    {error || (images.length === 0 && hasMore === true) ? <div className='error'>Something went wrong...</div>
+                        : <div className='results'>
 
-                            </div>
 
-                        </div>
-                    
-                    )})
-                }
-                
 
-                </div>}
+                            {images.map((image) => {
+                                return (
 
-                { images.length!==0 && <div className='load-more'
-                onClick={(e) => {
-                    setPage(page + 1);
-                    handleSubmit(e);
-                }} >Load More
-                </div>}
+                                    <div className='image-container' key={image.id}>
+                                        <div className='image'>
+                                            <img src={image.urls.small} alt={image.alt_description} />
+                                        </div>
+                                        <div className='text'>
+                                            <h2>{image.alt_description}</h2>
+                                            <p>By: {image.user.name}</p>
+                                            <a href={image.urls.raw}>Download</a>
 
-                    
+                                        </div>
+
+                                    </div>
+
+                                )
+                            })
+                            }
+
+
+                        </div>}
+
+                    {images.length !== 0 && <div className='load-more'
+                        onClick={(e) => {
+                            setPage(page + 1);
+                            handleSubmit(e);
+                        }} >Load More
+                    </div>}
+
+
                 </>
-                
-                
-                }
-                
-                
-    
-      
-    </div>
-  )
+
+
+            }
+
+
+
+
+        </div>
+    )
 }
 
 export default Home
